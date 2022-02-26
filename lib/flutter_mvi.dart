f2da14a41b2b5a52e7967f6b6a555110d32edf62
irakli dadiani <irakli.dadiani@changeinvest.com>
2022-02-26 13:25:47 +0200

    add exports

--- lib/flutter_mvi.dart
+++ lib/flutter_mvi.dart
@@ -1,1 +1,9 @@
 library flutter_mvi;
+
+export 'src/binder/binder.dart';
+export 'src/binder/bound_widget.dart';
+export 'src/element/elements.dart';
+export 'src/feature/mvi_feature.dart';
+export 'src/feature/reducer_feature.dart';
+export 'src/utils/extensions.dart';
+export 'src/utils/disposable.dart';
