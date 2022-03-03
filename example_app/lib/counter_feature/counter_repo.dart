7a2ec9c3dadfbd967e1b7dc084ed4bf09ce56d55
Irakli Dadiani <irakli.dadiani@changeinvest.com>
2022-03-03 18:40:40 +0200

    Add an example app (#1)
    * add example app
    
    * add mvi to example app

---
+++ example_app/lib/counter_feature/counter_repo.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+class CounterRepo {
+  Future<int> getInt() {
+    Random random = Random();
+    var delay = random.nextInt(5);
+    return Future.delayed(Duration(seconds: delay), () => random.nextInt(10));
+  }
+}
---
+++ example_app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:counter/counter_feature/counter_binder.dart';
+import 'package:counter/counter_feature/counter_feature.dart';
+import 'package:counter/counter_feature/counter_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mvi/flutter_mvi.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final binder = CounterBinder(CounterFeature(CounterRepo()));
+
+    return MaterialApp(
+      title: 'Flutter Mvi Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(binder: binder),
+    );
+  }
+}
+
+class HomePage extends BoundWidget<CounterUIState, CounterUIEvent> {
+  const HomePage({Key? key, required Binder<CounterUIState, CounterUIEvent> binder}) : super(key: key, binder: binder);
+
+  @override
+  Widget builder(BuildContext context, Binder<CounterUIState, CounterUIEvent> binder) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Mvi Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            binder.stateBuilder((context, snapshot) {
+              var counter = snapshot.data?.counter;
+              return Text(counter.toString());
+            }),
+            binder.stateBuilder((context, snapshot) {
+              var loading = snapshot.data?.loading;
+              return Text(loading.toString());
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          binder.call(PlusClicked());
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
